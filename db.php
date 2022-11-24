90243c54415c0c66308e23476e1d2803601b2db6
kendal-jc <kendalcalero864@gmail.com>
2022-11-23 19:10:49 -0600

    commit

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php 
+    namespace Medoo;
+    require 'Medoo.php';
+
+    if(!isset($database)){
+        $database = new Medoo([
+            // [required]
+            'type' => 'mysql',
+            'host' => 'db4free.net:3306',
+            'database' => 'recipess',
+            'username' => 'admimm',
+            'password' => 'Sark709909'
+        ]);
+    }
+?>
